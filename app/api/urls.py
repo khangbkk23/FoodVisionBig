b08a128aa516e970f13c29e9c838233abfd80573
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2026-03-08 13:51:55 +0700

    update

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('model_services.urls')), 
+    path('', include('model_services.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
